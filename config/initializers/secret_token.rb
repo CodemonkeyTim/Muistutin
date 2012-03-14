7a6ba08af5d35e80aeb634c6b4181220047e9427
Teemu Vainio <tim@silvertreksystems.com>
2012-03-14 10:38:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muistutin::Application.config.secret_token = '8fb85cde30ed0ca1efe0ff2ddf8cac9dc7c01d7ea2a8262140a704b1ec67e8c6c271fc16bc4b19749e9df3073d328575e4bd19dac51a6041b4c8be0264654833'
