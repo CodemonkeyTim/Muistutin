dfc75dd3247e865c06c893ae01b715bf339d8be8
Teemu Vainio <tim@silvertreksystems.com>
2012-03-16 06:16:58 -0700

    Stoof

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,14 @@ role :db,  "lassinmuistutin.myeffect.net", :primary => true # This is where Rail
 #role :db,  "your slave db-server here"
 
 set :username, "teemu"
+set :scm_passphrase, "Ticnbi'89"
+set :scm_verbose, true
+set :use_sudo, false
+
 set :deploy_to, "~/rails/Muistutin"
+set :ssh_options, {:forward_agent => true}
 set :deploy_via, :remote_cache
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
