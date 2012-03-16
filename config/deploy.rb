16925b4839f2046a29ff1a346faf8ec2e8bc184e
Teemu Vainio <tim@silvertreksystems.com>
2012-03-16 09:07:26 -0700

    Stoooooof:

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,18 +5,19 @@ set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :branch, "production"
 
-role :web, "lassinmuistutin.myeffect.net"                          # Your HTTP server, Apache/etc
-role :app, "lassinmuistutin.myeffect.net"                          # This may be the same as your `Web` server
-role :db,  "lassinmuistutin.myeffect.net", :primary => true # This is where Rails migrations will run
+role :web, "192.168.0.20"                          # Your HTTP server, Apache/etc
+role :app, "192.168.0.20"                          # This may be the same as your `Web` server
+role :db,  "192.168.0.20", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set :username, "teemu"
 set :scm_passphrase, "Ticnbi'89"
-set :scm_verbose, true
-set :use_sudo, false
+#set :scm_verbose, true
+set :use_sudo, true
 
-set :deploy_to, "~/rails/Muistutin"
+set :deploy_to, "/home/teemu/rails/Muistutin"
 set :ssh_options, {:forward_agent => true}
+
 set :deploy_via, :remote_cache
 
 
